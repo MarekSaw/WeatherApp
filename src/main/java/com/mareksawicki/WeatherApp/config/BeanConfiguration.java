2ca8ab9671f6a1e420883ce0711bd9fc26937b56
Marek <marek.sawick@gmail.com>
2021-01-29 00:11:53 +0100

    BeanConfiguration added

---
+++ src/main/java/com/mareksawicki/WeatherApp/config/BeanConfiguration.java
@@ -0,0 +1,37 @@
+package com.mareksawicki.WeatherApp.config;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.mareksawicki.WeatherApp.enums.WeatherSource;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+
+@Configuration
+public class BeanConfiguration {
+
+  @Bean(name = "WEATHER_BIT")
+  public WeatherSource getWeatherBit() {
+    return WeatherSource.WEATHER_BIT;
+  }
+
+  @Bean(name = "TOMORROW")
+  public LocalDate getTomorrowDate() {
+    return LocalDate.now().plusDays(1);
+  }
+
+  @Bean(name = "TODAY_MIDNIGHT")
+  public LocalDateTime getTodayMidnight() {
+    return LocalDate.now().atStartOfDay();
+  }
+
+  @Bean
+  public RestTemplate restTemplate(RestTemplateBuilder builder) {
+    return builder.build();
+  }
+
+}
