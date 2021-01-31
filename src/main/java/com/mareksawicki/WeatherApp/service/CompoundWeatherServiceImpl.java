7134159fe4873ad61c267adc941ba358d2e355a8
Marek <marek.sawick@gmail.com>
2021-01-31 22:43:31 +0100

    Getting average forecast parameters implemented

---
+++ src/main/java/com/mareksawicki/WeatherApp/service/CompoundWeatherServiceImpl.java
@@ -0,0 +1,55 @@
+package com.mareksawicki.WeatherApp.service;
+
+import com.mareksawicki.WeatherApp.entity.WeatherForecast;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service("compoundWeatherService")
+public class CompoundWeatherServiceImpl implements WeatherService {
+
+  private final LocalDate TOMORROW;
+  private final List<WeatherService> serviceList;
+
+  public CompoundWeatherServiceImpl(LocalDate tomorrow, @Qualifier("weatherBitService") WeatherService weatherBit, @Qualifier("openWeatherService") WeatherService openWeather) {
+    TOMORROW = tomorrow;
+    this.serviceList = List.of(weatherBit, openWeather);
+  }
+
+  @Override
+  public WeatherForecast getForecast(String city) {
+    return getForecast(city, TOMORROW);
+  }
+
+  @Override
+  public WeatherForecast getForecast(String city, LocalDate date) {
+    return getAverage(serviceList.stream()
+      .map(weatherService -> weatherService.getForecast(city, date))
+      .collect(Collectors.toList()));
+  }
+
+  @Override
+  public WeatherForecast getForecast(double lat, double lon) {
+    return getForecast(lat, lon, TOMORROW);
+  }
+
+  @Override
+  public WeatherForecast getForecast(double lat, double lon, LocalDate date) {
+    return getAverage(serviceList.stream()
+    .map(weatherService -> weatherService.getForecast(lat, lon, date))
+    .collect(Collectors.toList()));
+  }
+
+  private WeatherForecast getAverage(List<WeatherForecast> list) {
+    return WeatherForecast.builder()
+      .temperature(list.stream().map(WeatherForecast::getTemperature).reduce(0.0, Double::sum) / list.size())
+      .pressure(list.stream().map(WeatherForecast::getPressure).reduce(0.0, Double::sum) / list.size())
+      .humidity(list.stream().map(WeatherForecast::getHumidity).reduce(0.0, Double::sum) / list.size())
+      .windSpeed(list.stream().map(WeatherForecast::getWindSpeed).reduce(0.0, Double::sum) / list.size())
+      .windDeg(list.stream().map(WeatherForecast::getWindDeg).reduce(0.0, Double::sum) / list.size())
+      .build();
+  }
+}
