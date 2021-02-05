2b6a3ed3d16fada3ebeb5d4176412ccabf8c0bec
Marek <marek.sawick@gmail.com>
2021-02-05 11:31:30 +0100

    Modules, favicon, config changes added

--- src/main/app/app-routing.module.ts
+++ src/main/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {MainNavComponent} from './components/main-nav/main-nav.component';
+import {AppComponent} from './app.component';
+import {HomeComponent} from './components/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent}
+];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/main/app/app.module.ts
+++ src/main/app/app.module.ts
@@ -3,14 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainNavComponent } from './components/main-nav/main-nav.component';
+import { HomeComponent } from './components/home/home.component';
+import {HttpClient, HttpClientModule, HttpHandler} from '@angular/common/http';
+import {ReactiveFormsModule} from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainNavComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
