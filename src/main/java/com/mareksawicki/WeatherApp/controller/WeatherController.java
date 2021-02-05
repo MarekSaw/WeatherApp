6a1f0e50e34120135419495d9f4e549fe4a65818
Marek <marek.sawick@gmail.com>
2021-02-05 13:32:32 +0100

    Pagination moved to new method

--- src/main/java/com/mareksawicki/WeatherApp/controller/WeatherController.java
+++ src/main/java/com/mareksawicki/WeatherApp/controller/WeatherController.java
@@ -1,6 +1,7 @@
 package com.mareksawicki.WeatherApp.controller;
 
 import com.mareksawicki.WeatherApp.entity.WeatherForecast;
+import com.mareksawicki.WeatherApp.exception.ForecastNotFoundException;
 import com.mareksawicki.WeatherApp.service.ForecastService;
 import com.mareksawicki.WeatherApp.service.WeatherService;
 import org.springframework.beans.factory.annotation.Qualifier;
@@ -13,7 +14,7 @@ import java.util.Objects;
 
 @RestController
 @CrossOrigin(origins = "http://localhost:4200")
-@RequestMapping("/weather-api/forecast")
+@RequestMapping("/weather-api")
 public class WeatherController {
 
   private final WeatherService weatherService;
@@ -24,25 +25,29 @@ public class WeatherController {
     this.forecastService = forecastService;
   }
 
-  @GetMapping
-  public ResponseEntity<?> getForecast(@RequestParam(required = false) String city,
+  @GetMapping("/forecast")
+  public ResponseEntity<?> getAllForecasts(@RequestParam(required = false) Integer page,
+                                              @RequestParam(required = false) Integer size) {
+    return ResponseEntity.ok(forecastService.getAllForecasts(page, size));
+  }
+
+  @GetMapping("/weather-forecast")
+  public ResponseEntity<?> getWeatherForecast(@RequestParam(required = false) String city,
                                               @RequestParam(required = false) Double lat,
                                               @RequestParam(required = false) Double lon,
-                                              @RequestParam(required = false) String date,
-                                              @RequestParam(required = false) Integer page,
-                                              @RequestParam(required = false) Integer size) {
+                                              @RequestParam(required = false) String date) {
     WeatherForecast weatherForecast;
     if (Objects.nonNull(city)) {
       weatherForecast = Objects.nonNull(date) ? weatherService.getForecast(city, LocalDate.parse(date)) : weatherService.getForecast(city);
     } else if (Objects.nonNull(lat) && Objects.nonNull(lon)) {
       weatherForecast = Objects.nonNull(date) ? weatherService.getForecast(lat, lon, LocalDate.parse(date)) : weatherService.getForecast(lat, lon);
     } else {
-      return ResponseEntity.ok(forecastService.getAllForecasts(page, size));
+      throw new ForecastNotFoundException("One of required parameters was not given");
     }
     return ResponseEntity.ok(weatherForecast);
   }
 
-  @DeleteMapping("/{id}")
+  @DeleteMapping("/forecast/{id}")
   public ResponseEntity<?> deleteForecast(@PathVariable Long id) {
     return forecastService.removeForecastById(id) ? ResponseEntity.accepted().build() : ResponseEntity.notFound().build();
   }
