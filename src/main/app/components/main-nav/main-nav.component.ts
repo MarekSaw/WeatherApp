35c6522100d551b4c1ae49a9b1008774d3772462
Marek <marek.sawick@gmail.com>
2021-02-08 11:55:42 +0100

    Click on background closes the nav bar added

--- src/main/app/components/main-nav/main-nav.component.ts
+++ src/main/app/components/main-nav/main-nav.component.ts
@@ -8,14 +8,22 @@ import * as $ from 'jquery';
 })
 export class MainNavComponent implements OnInit {
 
+  isSideNavOpen: boolean;
+
   constructor() { }
 
   ngOnInit(): void {
     $('.side-nav-content').hide();
+    $('.content').on('click', () => {
+      if (this.isSideNavOpen) {
+        this.toggleNav();
+      }
+    });
   }
 
   public toggleNav(): void {
     $('.side-nav-content').animate({width: 'toggle', opacity: 'toggle'});
+    this.isSideNavOpen = !this.isSideNavOpen;
   }
 
 }
