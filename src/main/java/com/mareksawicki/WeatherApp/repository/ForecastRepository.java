e9d7eb0669592e2956bb17bf20c300209cf45fd6
Marek <marek.sawick@gmail.com>
2021-02-16 13:16:09 +0100

    update

--- src/main/java/com/mareksawicki/WeatherApp/repository/ForecastRepository.java
+++ src/main/java/com/mareksawicki/WeatherApp/repository/ForecastRepository.java
@@ -2,10 +2,12 @@ package com.mareksawicki.WeatherApp.repository;
 
 import com.mareksawicki.WeatherApp.entity.Forecast;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 
+@Repository
 public interface ForecastRepository extends JpaRepository<Forecast, Long> {
 
   Forecast findByLocalizationAndForecastDateAndForecastAcquiredDateIsAfter(String localization, LocalDate date, LocalDateTime todayMidnight);
