759c465b13bdbdd846f6d2d81d923bc973d620a3
Marek <marek.sawick@gmail.com>
2021-02-18 19:39:11 +0100

    Validating form improved

--- src/main/app/components/register/register.component.ts
+++ src/main/app/components/register/register.component.ts
@@ -1,10 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import {AuthService} from '../../service/auth.service';
-import {FormBuilder, FormControl, FormGroup, PatternValidator, Validators} from '@angular/forms';
+import {FormControl, FormGroup, Validators} from '@angular/forms';
 
 
 function passwordMatchValidator(g: FormGroup): any {
-  return g.get('passwordForm').value === g.get('passwordConfirmForm').value
+  return g.get('password').value === g.get('passwordConfirm').value
     ? null : {mismatch: true};
 }
 
@@ -16,19 +16,19 @@ function passwordMatchValidator(g: FormGroup): any {
 export class RegisterComponent implements OnInit {
 
   registerGroup = new FormGroup({
-    usernameForm: new FormControl('', [
+    username: new FormControl('', [
       Validators.required,
       Validators.pattern('^(?![_ -])(?:(?![_ -]{2})[\\w -]){4,20}(?<![_ -])$'),
     ]),
-    emailForm: new FormControl('', [
+    email: new FormControl('', [
       Validators.required,
       Validators.pattern('^([a-zA-Z0-9_\\-\\.]+)@([a-zA-Z0-9_\\-\\.]+)\\.([a-zA-Z]{2,5})$'),
     ]),
-    passwordForm: new FormControl('', [
+    password: new FormControl('', [
       Validators.required,
       Validators.pattern('^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-.,_]).{8,}$'),
     ]),
-    passwordConfirmForm: new FormControl('', [
+    passwordConfirm: new FormControl('', [
       Validators.required,
       Validators.pattern('^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-.,_]).{8,}$'),
     ])}, passwordMatchValidator);
@@ -38,16 +38,50 @@ export class RegisterComponent implements OnInit {
   errorMessage = '';
 
 
-  constructor(private authService: AuthService, private formBuilder: FormBuilder) {
+  constructor(private authService: AuthService) {
   }
 
   ngOnInit(): void {
   }
 
   onSubmit(): void {
-    this.authService.register(this.registerGroup.get('usernameForm').value,
-                              this.registerGroup.get('emailForm').value,
-                              this.registerGroup.get('passwordForm').value).subscribe(
+    if (this.registerGroup.valid) {
+      this.register();
+    } else {
+      if (!this.registerGroup.get('username').valid) {
+        document.getElementById('username').classList.add('is-invalid');
+      }
+      if (!this.registerGroup.get('email').valid) {
+        document.getElementById('email').classList.add('is-invalid');
+      }
+      if (!this.registerGroup.get('password').valid) {
+        document.getElementById('password').classList.add('is-invalid');
+      }
+      if (!this.registerGroup.get('passwordConfirm').valid) {
+        document.getElementById('passwordConfirm').classList.add('is-invalid');
+      }
+    }
+  }
+
+  checkForm(): void {
+    if (this.registerGroup.get('username').valid) {
+      document.getElementById('username').classList.remove('is-invalid');
+    }
+    if (this.registerGroup.get('email').valid) {
+      document.getElementById('email').classList.remove('is-invalid');
+    }
+    if (this.registerGroup.get('password').valid) {
+      document.getElementById('password').classList.remove('is-invalid');
+    }
+    if (this.registerGroup.get('passwordConfirm').valid) {
+      document.getElementById('passwordConfirm').classList.remove('is-invalid');
+    }
+  }
+
+  public register(): void {
+    this.authService.register(this.registerGroup.get('username').value,
+      this.registerGroup.get('email').value,
+      this.registerGroup.get('password').value).subscribe(
       data => {
         console.log(data);
         this.isSuccessful = true;
