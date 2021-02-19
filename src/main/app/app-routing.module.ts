6edfd6b9c75200031b3cc0584efb6774587dbefb
Marek <marek.sawick@gmail.com>
2021-02-19 16:11:30 +0100

    Profile component added

--- src/main/app/app-routing.module.ts
+++ src/main/app/app-routing.module.ts
@@ -8,6 +8,7 @@ import {ContactComponent} from './components/contact/contact.component';
 import {AboutComponent} from './components/about/about.component';
 import {LoginComponent} from './components/login/login.component';
 import {RegisterComponent} from './components/register/register.component';
+import {ProfileComponent} from './components/profile/profile.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
@@ -16,7 +17,8 @@ const routes: Routes = [
   { path: 'contact', component: ContactComponent },
   { path: 'about', component: AboutComponent },
   { path: 'login', component: LoginComponent },
-  { path: 'register', component: RegisterComponent }
+  { path: 'register', component: RegisterComponent },
+  { path: 'profile', component: ProfileComponent }
 ];
 
 
