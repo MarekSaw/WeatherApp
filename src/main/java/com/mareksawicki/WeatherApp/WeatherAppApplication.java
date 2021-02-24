2fd00904f80aa84c097bf8cff599caaf555fbf64
Marek <marek.sawick@gmail.com>
2021-02-24 10:04:36 +0100

    Cleaning stuff

--- src/main/java/com/mareksawicki/WeatherApp/WeatherAppApplication.java
+++ src/main/java/com/mareksawicki/WeatherApp/WeatherAppApplication.java
@@ -3,10 +3,11 @@ package com.mareksawicki.WeatherApp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class WeatherAppApplication {
 
-    public static void main(String[] args) {
+  public static void main(String[] args) {
         SpringApplication.run(WeatherAppApplication.class, args);
     }
 
