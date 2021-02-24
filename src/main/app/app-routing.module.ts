624f27df95453cd389754cf85d69cd1c6314cf43
Marek <marek.sawick@gmail.com>
2021-02-24 13:53:13 +0100

    HashLocationStrategy turned on - because of problem with reload after window.location.reload

--- src/main/app/app-routing.module.ts
+++ src/main/app/app-routing.module.ts
@@ -29,7 +29,7 @@ const routes: Routes = [
 
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, { useHash: true })],
   providers: [ForecastResolver],
   exports: [RouterModule]
 })
